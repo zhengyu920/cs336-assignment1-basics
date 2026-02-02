60bb84c165beadab0abb10af965b450f971bacd3
Yu Zheng <zhengyu920@gmail.com>
2026-02-01 23:59:56 -0800

    implemented training loop and verified with bpe_example.txt

--- tokenizer/bpe_trainer.py
+++ tokenizer/bpe_trainer.py
@@ -3,12 +3,12 @@ from pathlib import Path
 import regex as re
 
 
-def find_max_bp(pretoken_count: dict):
+def find_max_bp(pretokenized_count):
     # Count byte pairs
     bp_counter = {}
-    for encoded_tuple, count in pretoken_count.items():
-        for i in range(len(encoded_tuple) - 1):
-            bp = (encoded_tuple[i], encoded_tuple[i+1])
+    for bytes_tuple, count in pretokenized_count.items():
+        for i in range(len(bytes_tuple) - 1):
+            bp = (bytes_tuple[i], bytes_tuple[i+1])
             bp_counter[bp] = bp_counter.get(bp, 0) + count
     max_bp, max_count = (0, 0), 0
     for bp, bp_count in bp_counter.items():
@@ -21,30 +21,27 @@ def find_max_bp(pretoken_count: dict):
     return max_bp, max_count
 
 
-def transform_and_count_pretoken(pretoken: list) -> dict:
-    pretoken_count = {}
-    for pt in pretoken:
-        encoded_tuple = tuple(pt.encode('utf-8'))
-        pretoken_count[encoded_tuple] = pretoken_count.get(
-            encoded_tuple, 0) + 1
-    return pretoken_count
+def concat_merge(merge):
+    part1 = bytes([merge[0]]) if isinstance(merge[0], int) else merge[0]
+    part2 = bytes([merge[1]]) if isinstance(merge[1], int) else merge[1]
+    return part1 + part2
 
 
-def merge_bp(pretoken: dict, bp_merge: tuple):
-    new_pretoken = {}
-    origin, target = bp_merge
-    for pt, count in pretoken.items():
+def merge_bp(pretokenized_count: dict[tuple[bytes], int], merge):
+    new_pretokenized_count = {}
+    merged_bytes = concat_merge(merge)
+    for pt, count in pretokenized_count.items():
         new_pt = []
         i = 0
         while i < len(pt):
-            if i < len(pt) - 1 and (pt[i], pt[i+1]) == origin:
-                new_pt.append(target)
+            if i < len(pt) - 1 and (pt[i], pt[i+1]) == merge:
+                new_pt.append(merged_bytes)
                 i = i + 2
             else:
                 new_pt.append(pt[i])
                 i = i + 1
-        new_pretoken[tuple(new_pt)] = count
-    return new_pretoken
+        new_pretokenized_count[tuple(new_pt)] = count
+    return new_pretokenized_count
 
 
 def remove_special_tokens(content: str, special_tokens: list[str]) -> list[str]:
@@ -55,16 +52,26 @@ def remove_special_tokens(content: str, special_tokens: list[str]) -> list[str]:
 PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 
 
-def pretokenizer(content: str, special_tokens: list[str]) -> dict[list[bytes], int]:
+def pretokenizer(content: str, special_tokens: list[str]) -> dict[tuple[bytes], int]:
     docs = remove_special_tokens(content, special_tokens)
     counter = {}
     for doc in docs:
         matches = re.finditer(PAT, doc)
 
         for match in matches:
-            text = match.group()
+            text = tuple(match.group().encode('utf8'))
             counter[text] = counter.get(text, 0) + 1
     return counter
+
+
+def init_vocab(special_tokens: list[str]) -> dict[int, bytes]:
+    vocab = {}
+    for i in range(256):
+        vocab[i] = bytes([i])
+    for st in special_tokens:
+        idx = len(vocab)
+        vocab[idx] = st.encode('utf8')
+    return vocab
 
 
 def train_bpe(
@@ -72,9 +79,32 @@ def train_bpe(
         vocab_size: int,
         special_tokens: list[str]
 ) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+    vocab = init_vocab(special_tokens)
+    merges = []
+
     path = Path(input_path)
     content = ''
     with path.open('+r') as f:
         content = f.read()
-    pretokenized_count = pretokenizer(content)
-    return {}, []
+    pretokenized_count = pretokenizer(content, special_tokens)
+    for _ in range(6):
+        max_bp, max_count = find_max_bp(pretokenized_count)
+        # only merge and update when max_count > 1.
+        if max_count > 1:
+            vocab_size = len(vocab)
+            vocab[vocab_size] = concat_merge(max_bp)
+            merges.append(max_bp)
+            pretokenized_count = merge_bp(pretokenized_count, max_bp)
+        else:
+            break
+    return vocab, merges
+
+
+if __name__ == "__main__":
+    # input_path = './data/TinyStoriesV2-GPT4-valid.txt'
+    input_path = './tokenizer/bpe_example.txt'
+    special_tokens = ['<|endoftext|>']
+    vocab_size = 1000
+    vocab, merges = train_bpe(input_path, vocab_size, special_tokens)
+    print(vocab)
+    print(merges)
