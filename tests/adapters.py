62b3e83b331fb270a4edee3e7d2e150e032fe02e
Yu Zheng <zhengyu920@gmail.com>
2026-02-04 21:20:31 -0800

    implemented from_files and adapter for tokenizer

--- tests/adapters.py
+++ tests/adapters.py
@@ -4,6 +4,7 @@ import os
 from collections.abc import Iterable
 from typing import IO, Any, BinaryIO
 from tokenizer import bpe_trainer
+from tokenizer import tokenizer
 
 import numpy.typing as npt
 import torch
@@ -560,7 +561,7 @@ def get_tokenizer(
     Returns:
         A BPE tokenizer that uses the provided vocab, merges, and special tokens.
     """
-    raise NotImplementedError
+    raise tokenizer.Tokenizer(vocab, merges, special_tokens)
 
 
 def run_train_bpe(
--- tokenizer/tokenizer.py
+++ tokenizer/tokenizer.py
@@ -1,4 +1,5 @@
-
+from pathlib import Path
+import pickle
 class Tokenizer:
     def __init__(self, vocab : dict[int, bytes], merges : list[tuple[bytes, bytes]], special_tokens :  list[str] | None=None):
         self._vocab = vocab
@@ -6,7 +7,14 @@ class Tokenizer:
         self._special_tokens = special_tokens
 
     def from_files(cls, vocab_filepath = str, merges_filepath = str, special_tokens : list[str] | None=None):
-        return
+        vocab_path = Path(vocab_filepath)
+        merges_path = Path(merges_filepath)
+        with open(vocab_path, 'rb') as f:
+            vocab = pickle.load(f)
+        with open(merges_path, 'rb') as f:
+            merges = pickle.load(f)
+
+        return Tokenizer(vocab, merges, special_tokens)
     
     def encode(self, text: str) -> list[int]:
         return []
