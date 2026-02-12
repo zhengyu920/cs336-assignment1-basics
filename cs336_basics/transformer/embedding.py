1a24442b8a2c7d6f56c0d91378ec0cdc9e8d58bd
Yu Zheng <zhengyu920@gmail.com>
2026-02-11 22:21:33 -0800

    Initial implementation of RoPE

--- cs336_basics/transformer/embedding.py
+++ cs336_basics/transformer/embedding.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn
 from cs336_basics.transformer import init_param
+import math
+from einops import rearrange, einsum
 
 
 class Embedding(nn.Module):
@@ -14,3 +16,28 @@ class Embedding(nn.Module):
 
     def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
         return self.embeddings[token_ids]
+
+
+class RoPE(nn.Module):
+    def __init__(self, theta: float, d_k: int, max_seq_len: int, device: torch.device | None = None):
+        super().__init__()
+        self.theta = theta
+        self.d_k = d_k
+        self.max_seq_len = max_seq_len
+        rotations = []
+        for i in range(max_seq_len):
+            r_i = []
+            for k in range(1, int(d_k/2) + 1):
+                theta_i_k = i / math.pow(theta, (2*k-2)/d_k)
+                r_i_k = torch.Tensor([
+                    [math.cos(theta_i_k), -math.sin(theta_i_k)],
+                    [math.sin(theta_i_k), math.cos(theta_i_k)]])
+                r_i.append(r_i_k)
+            rotations.append(torch.block_diag(*r_i))
+        self.rotations = torch.stack(rotations)
+
+    def forward(self, x: torch.Tensor, token_positions: torch.Tensor) -> torch.Tensor:
+        rotations = self.rotations[token_positions, :, :]
+        # rotations = rearrange(
+        #     rotations, "... seq_len d_k_1 d_k_2 -> ... seq_len d_k_2 d_k_1")
+        return einsum(x, rotations, "... seq_len d_k, ... seq_len d_k d_k -> ... seq_len d_k")
--- tests/adapters.py
+++ tests/adapters.py
@@ -216,7 +216,8 @@ def run_rope(
     Returns:
         Float[Tensor, " ... sequence_length d_k"]: Tensor with RoPEd input.
     """
-    raise NotImplementedError
+    rope = embedding.RoPE(theta, d_k, max_seq_len)
+    return rope(in_query_or_key, token_positions)
 
 
 def run_transformer_block(
