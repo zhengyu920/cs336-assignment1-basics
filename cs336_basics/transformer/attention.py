2a1a9dc5eb18dbde655c89660aeaa328422e08f6
Yu Zheng <zhengyu920@gmail.com>
2026-02-14 19:23:15 -0800

    Refactor multihead attention to add RoPE

--- cs336_basics/transformer/attention.py
+++ cs336_basics/transformer/attention.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 from cs336_basics.transformer.functional import Softmax
-from cs336_basics.transformer.linear import Linear
+from cs336_basics.transformer.embedding import RoPE
 from cs336_basics.transformer.init_param import init_param
 from jaxtyping import Bool, Float, Int
 import math
@@ -31,7 +31,7 @@ class ScaledDotProductAttention(nn.Module):
 
 
 class MultiHeadSelfAttention(nn.Module):
-    def __init__(self, d_model: int, num_heads: int, device=None):
+    def __init__(self, d_model: int, num_heads: int, theta: float = 0, device=None):
         super().__init__()
         self.d_model = d_model
         self.num_heads = num_heads
@@ -46,8 +46,14 @@ class MultiHeadSelfAttention(nn.Module):
         self.o_proj_weight = init_param(
             self.d_k * self.num_heads, self.d_model)
         self.attention = ScaledDotProductAttention()
+        self.theta = theta
+        self.rope = RoPE(self.theta, self.d_k, 1000,
+                         device) if self.theta != 0 else None
 
-    def forward(self, x: Float[torch.Tensor, "... seq d_model"]) -> Float[torch.Tensor, " ... sequence_length d_out"]:
+    def forward(self, x: Float[torch.Tensor, "... seq d_model"],
+                token_positions: Int[torch.Tensor,
+                                     " ... sequence_length"] | None = None
+                ) -> Float[torch.Tensor, " ... sequence_length d_out"]:
         Q = einsum(x, self.q_proj_weight,
                    "... seq d_model_in, d_model_out d_model_in -> ... seq d_model_out")
         K = einsum(x, self.k_proj_weight,
@@ -56,6 +62,10 @@ class MultiHeadSelfAttention(nn.Module):
                    "... seq d_model_in, d_model_out d_model_in -> ... seq d_model_out")
         seq = x.shape[-2]
         mask = torch.tril(torch.ones(seq, seq)).bool()
-        attentions = [self.attention(Q[..., i * self.d_k: (i+1)*self.d_k], K[..., i * self.d_k: (i+1)*self.d_k], V[..., i * self.d_k: (i+1)*self.d_k], mask)
-                      for i in range(self.num_heads)]
+        attentions = []
+        for i in range(self.num_heads):
+            Qi = Q[..., i * self.d_k: (i+1)*self.d_k]
+            Ki = K[..., i * self.d_k: (i+1)*self.d_k]
+            Vi = V[..., i * self.d_k: (i+1)*self.d_k]
+            attentions.append(self.attention(Qi, Ki, Vi, mask))
         return einsum(torch.concat(attentions, dim=-1), self.o_proj_weight, "... seq d_model_in, d_model_out d_model_in -> ... seq d_model_out")
