70e6cb5a18ab3c87bcac57bd57b1887a6e928d30
Yu Zheng <zhengyu920@gmail.com>
2026-07-19 00:07:52 -0700

    train bpe

--- tests/adapters.py
+++ tests/adapters.py
@@ -9,6 +9,8 @@ import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
+from train_bpe import train_bpe
+
 
 def run_linear(
     d_in: int,
@@ -589,4 +591,4 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    raise NotImplementedError
+    return train_bpe(input_path, vocab_size, special_tokens)
