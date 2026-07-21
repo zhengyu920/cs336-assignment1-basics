f414f3759a5c6a1ce83570d2fc09de31c362b2ed
Yu Zheng <zhengyu920@gmail.com>
2026-07-21 00:01:35 -0700

    train bpe owt

--- train_bpe_owt.py
+++ train_bpe_owt.py
@@ -1,14 +1,14 @@
 from train_bpe import train_bpe
 import pickle
 
-MAX_VOCAB_SIZE = 10000
+MAX_VOCAB_SIZE = 32000
 S_T = ['<|endoftext|>']
 
-FILE_PATH = 'data/TinyStoriesV2-GPT4-train.txt'
+FILE_PATH = 'data/owt_train.txt'
 
 vocab, merges = train_bpe(FILE_PATH, MAX_VOCAB_SIZE, S_T, num_processes=8)
 
-with open('bpe_ts_vocab.pkl', 'wb') as f:
+with open('bpe_owt_vocab.pkl', 'wb') as f:
     pickle.dump(vocab, f)
-with open('bpe_ts_merges.pkl', 'wb') as f:
+with open('bpe_owt_merges.pkl', 'wb') as f:
     pickle.dump(merges, f)
